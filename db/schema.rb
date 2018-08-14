9660a2c795381eb6bbb8d295803db8d015ca5a6e
Alando Appling <alandoappling@gmail.com>
2018-08-14 12:56:17 -0700

    Added disqus

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_12_043438) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
